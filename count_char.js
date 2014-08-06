d58915397cdfae3e3631b453a92a0505a026d6c2
Joseph Jo <joseph.jo@coxinc.com>
2014-08-06 12:19:40 -0400

    Simple exercise to create a function that counts chars in a given string

---
+++ count_char.js
@@ -0,0 +1,15 @@
+// Function to get count of a char in a string
+function count_char(str, char) {
+    var count = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count++;
+        }
+    };
+
+    return count;
+}
+
+console.log(count_char("krazyklown", "k")); // 2
+console.log(count_char("zzzzzzzzzzzzzzzzzzz", "a")); // 0
