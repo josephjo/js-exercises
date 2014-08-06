315e46560f209553d80fef7e3a80a2a2d256c4a6
Joseph Jo <joseph.jo@coxinc.com>
2014-08-06 12:03:18 -0400

    Alternative way of finding is_even using recursion (thought exercise)

--- is_even.js
+++ is_even.js
@@ -7,7 +7,23 @@ function is_even(number) {
     }
 }
 
+// Use recursion to determine if a number is even
+function is_even_recursion(number) {
+    number = -number > 0 ? -number : number; // Make sure we're not working with negative numbers
+
+    if (number === 0) {
+        return true;
+    } else if (number === 1) {
+        return false;
+    } else {
+        is_even_recursion(number -= 2);
+    }
+}
+
 
 console.log(is_even(50)); // true
 console.log(is_even(75)); // false
 console.log(is_even(-1)); // false
+console.log(is_even_recursion(50)); // true
+console.log(is_even_recursion(75)); // false
+console.log(is_even_recursion(-1)); // false
