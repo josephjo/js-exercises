9ec6690600aa26c6ae5228ac1b3bb0ed2a7f66f5
Joseph Jo <joseph.jo@coxinc.com>
2014-08-07 09:34:55 -0400

    Add optional step argument to range function

--- range_sum.js
+++ range_sum.js
@@ -1,7 +1,12 @@
 // Range function that lists numbers from start arg to end arg
-function range(start, end) {
-    arr = [];
-    for (var i = start; i <= end; i++) {
+function range(start, end, step) {
+    var arr = [], counter = 1;
+
+    if (arguments.length === 3) {
+        counter = step;
+    }
+
+    for (var i = start; i <= end; i += counter) {
         arr.push(i);
     }
 
@@ -17,10 +22,15 @@ function sum(arr) {
     for (var num in arr) {
         sum += Number(arr[num]);
     }
+
     return sum;
 }
 
 
 
 console.log(range(1, 10)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(1, 10, 2)); // [1, 3, 5, 7, 9]
+console.log(range(1, 10, 5)); // [1, 6]
+console.log(range(5, 2)); // [5, 4, 3, 2]
 console.log(sum(range(1, 10))); // 55
+console.log(sum(range(1, 10, 2))); // 25
