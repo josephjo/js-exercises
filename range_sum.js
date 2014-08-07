20516f149a2bdcb91c11cc60d5a4d8ad25a78842
Joseph Jo <joseph.jo@coxinc.com>
2014-08-07 09:31:39 -0400

    Forgot to use semicolons; too used to Python

--- range_sum.js
+++ range_sum.js
@@ -1,11 +1,11 @@
 // Range function that lists numbers from start arg to end arg
 function range(start, end) {
-    arr = []
+    arr = [];
     for (var i = start; i <= end; i++) {
         arr.push(i);
     }
 
-    return arr
+    return arr;
 }
 
 
@@ -15,13 +15,12 @@ function sum(arr) {
     console.log(arr);
 
     for (var num in arr) {
-        console.log(arr[num])
         sum += Number(arr[num]);
     }
-    return sum
+    return sum;
 }
 
 
 
-console.log(range(1, 10));
+console.log(range(1, 10)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(sum(range(1, 10))); // 55
