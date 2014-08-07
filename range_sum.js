f9cfe0ba055943522c800d1d1399ea4aaf4e6314
Joseph Jo <joseph.jo@coxinc.com>
2014-08-07 09:04:02 -0400

    Add sum function to range_sum to get total of array

--- range_sum.js
+++ range_sum.js
@@ -9,4 +9,19 @@ function range(start, end) {
 }
 
 
+// Sum function that takes an array of numbers and returns sum
+function sum(arr) {
+    var sum = 0;
+    console.log(arr);
+
+    for (var num in arr) {
+        console.log(arr[num])
+        sum += Number(arr[num]);
+    }
+    return sum
+}
+
+
+
 console.log(range(1, 10));
+console.log(sum(range(1, 10))); // 55
