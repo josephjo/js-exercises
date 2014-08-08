84cea8ff629d5ca226ab4a175caa04c71f181d71
Joseph Jo <joseph.jo@coxinc.com>
2014-08-08 09:25:50 -0400

    Two ways to reverse an array: which is better?

---
+++ reverse_array.js
@@ -0,0 +1,18 @@
+// Reverse a given array
+function reverse_array(arr) {
+    var new_arr = arr.reverse();
+    return new_arr;
+}
+
+
+// Reverse a given array
+var reverse_array_in_place = function(arr) {
+    var new_arr = arr.reverse();
+}
+
+
+
+console.log(reverse_array(['A', 'B', 'C'])); // ['C', 'B', 'A'];
+var array_value = ['A', 'B', 'C'];
+reverse_array_in_place(array_value);
+console.log(array_value); // ['C', 'B', 'A'];
