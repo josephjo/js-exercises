82ceb7c2ab004a417dd7c7bf88eca23579aefff5
Joseph Jo <joseph.jo@coxinc.com>
2014-09-10 13:21:37 -0400

    Get user input for cost, miles, and fees (JS version)

--- calc_air_miles.js
+++ calc_air_miles.js
@@ -1,10 +1,14 @@
 // Calculate cost per mile for miles needed for a free flight minus any fees
-function calc_air_miles(cost, miles, fees) {
-    var total;
+function calc_air_miles() {
+    var cost = prompt("Amount you would pay for the flight: $"),
+        miles = prompt("Amount of miles it costs: "),
+        fees = prompt("Amount of fees associated with redeeming with miles: $"),
+        total;
+
     total = ((cost - fees) / miles) * 100;
 
     return "You're getting " + total.toFixed(2) + " cents per mile for a $" + cost + " ticket.";
 }
 
-console.log(calc_air_miles(300, 25000, 20)); // 1.12 cpm
-console.log(calc_air_miles(16200, 200000, 300)); // 7.95 cpm
+
+console.log(calc_air_miles());
