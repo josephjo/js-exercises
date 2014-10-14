fae44100ef86e468240fb360abaf651ff002d3b5
Joseph Jo <joseph.jo@coxinc.com>
2014-10-14 13:41:31 -0400

    Add swith statement to determine animal

--- chinese_zodiac.js
+++ chinese_zodiac.js
@@ -3,7 +3,49 @@ function zodiac(year) {
     var num = year % 12,
         animal = '';
 
+    switch (num) {
+        case 0:
+            animal = 'Monkey';
+            break;
+        case 1:
+            animal = 'Rooster';
+            break;
+        case 2:
+            animal = 'Dog';
+            break;
+        case 3:
+            animal = 'Pig';
+            break;
+        case 4:
+            animal = 'Rat';
+            break;
+        case 5:
+            animal = 'Ox';
+            break;
+        case 6:
+            animal = 'Tiger';
+            break;
+        case 7:
+            animal = 'Rabbit';
+            break;
+        case 8:
+            animal = 'Dragon';
+            break;
+        case 9:
+            animal = 'Snake';
+            break;
+        case 10:
+            animal = 'Horse';
+            break;
+        case 11:
+            animal = 'Goat';
+            break;
+        default:
+            animal = 'No animal found';
+    }
+
     console.log(year + ": " + animal);
 }
+
 
 zodiac(1927);
