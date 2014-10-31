b9a6d298105ad157126ee7d892ec4fecbf840264
Joseph Jo <joseph.jo@coxinc.com>
2014-10-31 09:58:55 -0400

    Use single quotes

--- youtube.js
+++ youtube.js
@@ -1,7 +1,7 @@
-// Watch a YouTube video without logging in by inserting "/v" after hostname
+// Watch a YouTube video without logging in by inserting '/v' after hostname
 (function() {
     var url = window.location;
-    if (url.hostname === "www.youtube.com") {
-        url.href = url.origin + "/v" + url.pathname + url.search;
+    if (url.hostname === 'www.youtube.com') {
+        url.href = url.origin + '/v' + url.pathname + url.search;
     }
 })();
