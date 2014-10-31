d6d110b39819bd1df7b9adbe84df3523f9a164b3
Joseph Jo <joseph.jo@coxinc.com>
2014-10-31 09:59:47 -0400

    Give the function a name

--- youtube.js
+++ youtube.js
@@ -1,7 +1,10 @@
 // Watch a YouTube video without logging in by inserting '/v' after hostname
-(function() {
+function youtube() {
     var url = window.location;
     if (url.hostname === 'www.youtube.com') {
         url.href = url.origin + '/v' + url.pathname + url.search;
     }
-})();
+}
+
+
+youtube();
