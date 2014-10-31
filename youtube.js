24fc367ebf8377279a07fa159ecfdf158a9bff9e
Joseph Jo <joseph.jo@coxinc.com>
2014-10-31 09:58:07 -0400

    Set window location to current URL plus the "/v"

--- youtube.js
+++ youtube.js
@@ -1,4 +1,6 @@
 // Watch a YouTube video without logging in by inserting "/v" after hostname
 (function() {
-    if (window.location.hostname === "www.youtube.com") {}
+    if (window.location.hostname === "www.youtube.com") {
+        window.location.href = window.location.origin + "/v" + window.location.pathname + window.location.search;
+    }
 })();
