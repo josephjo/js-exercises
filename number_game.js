2fc9565747fdf7623b705ec30a3a7d5cf323d5fd
Joseph Jo <joseph@josephjo.com>
2015-02-13 20:44:15 -0500

    If you're wrong, then try again with a hint

--- number_game.js
+++ number_game.js
@@ -2,6 +2,14 @@ var number_game = function(num) {
     var real_num = Math.round(Math.random() * num + 1),
         guess = prompt("Guess a number!");
 
+    while(parseInt(guess) !== real_num) {
+        if (parseInt(guess) > real_num) {
+            guess = prompt("Guess lower.")
+        } else if (parseInt(guess) < real_num) {
+            guess = prompt("Guess higher.")
+        }
+    }
+
     if (parseInt(guess) === real_num) {
         alert("Correct! The number is " + real_num + ".");
     }
