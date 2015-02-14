5d959df7035452d3492349c11ba9aae3b1134e63
Joseph Jo <joseph@josephjo.com>
2015-02-13 20:44:58 -0500

    Use singl quotes instead of double

--- number_game.js
+++ number_game.js
@@ -1,17 +1,17 @@
 var number_game = function(num) {
     var real_num = Math.round(Math.random() * num + 1),
-        guess = prompt("Guess a number!");
+        guess = prompt('Guess a number!');
 
     while(parseInt(guess) !== real_num) {
         if (parseInt(guess) > real_num) {
-            guess = prompt("Guess lower.")
+            guess = prompt('Guess lower.')
         } else if (parseInt(guess) < real_num) {
-            guess = prompt("Guess higher.")
+            guess = prompt('Guess higher.')
         }
     }
 
     if (parseInt(guess) === real_num) {
-        alert("Correct! The number is " + real_num + ".");
+        alert('Correct! The number is ' + real_num + '.');
     }
 }
 
